38d9530f54bc68aa8d6245d13fdfff13668ab7d3
SamGerber <gerbyxvi@gmail.com>
2016-01-22 13:24:00 -0800

    Implement Associations, extending SQLObject

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -10,34 +10,50 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options.fetch(:foreign_key) { "#{name}_id".to_sym }
+    @primary_key = options.fetch(:primary_key) { :id }
+    @class_name  = options.fetch(:class_name)  { name.to_s.camelcase }
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options.fetch(:foreign_key) { "#{self_class_name.underscore}_id".to_sym }
+    @primary_key = options.fetch(:primary_key) { :id }
+    @class_name  = options.fetch(:class_name)  { name.to_s.camelcase.singularize }
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      value_of_foreign_key = self.send(options.foreign_key)
+      target_class = options.model_class
+      target_class.where(options.primary_key => value_of_foreign_key).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      value_of_primary_key = self.send(options.primary_key)
+      target_class = options.model_class
+      target_class.where(options.foreign_key => value_of_primary_key)
+    end
   end
 
   def assoc_options
@@ -46,5 +62,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
