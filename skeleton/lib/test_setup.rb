a77f80cadd92cd00652e43317b473f329b815e84
SamGerber <gerbyxvi@gmail.com>
2016-01-22 18:08:10 -0800

    Write code to setup pry sandbox

---
+++ skeleton/lib/test_setup.rb
@@ -0,0 +1,21 @@
+class Cat < SQLObject
+  belongs_to :human, foreign_key: :owner_id
+  validates :name, presence: true, uniqueness: true
+
+  finalize!
+end
+
+class Human < SQLObject
+  self.table_name = 'humans'
+
+  has_many :cats, foreign_key: :owner_id
+  belongs_to :house
+
+  finalize!
+end
+
+class House < SQLObject
+  has_many :humans
+
+  finalize!
+end
