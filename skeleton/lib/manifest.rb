ff5ba16c67c392736e694b908241f6601ab8f398
SamGerber <gerbyxvi@gmail.com>
2016-01-22 18:07:29 -0800

    Add manifest file

---
+++ skeleton/lib/manifest.rb
@@ -0,0 +1,16 @@
+# require 'byebug'
+require 'active_support/inflector'
+
+require_relative 'db_connection'
+require_relative '01_sql_object'
+require_relative '02_searchable'
+require_relative '03_associatable'
+require_relative '04_associatable2'
+
+require_relative 'validatable'
+
+require_relative 'errors'
+require_relative 'exceptions'
+require_relative 'core_extension'
+
+require_relative 'test_setup'
